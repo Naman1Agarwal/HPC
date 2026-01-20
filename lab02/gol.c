4a68c860b919ec817816897cefa1f12cce4586b8
Naman1Agarwal <m270036@usna.edu>
2026-01-20 09:35:33 -0500

    feat: added lab2

---
+++ lab02/gol.c
@@ -0,0 +1,278 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <stdint.h>
+
+typedef uint8_t value_t;
+typedef uint32_t index_t;
+typedef int32_t  init_t;
+
+typedef struct matrix{
+    index_t rows;
+    index_t cols;
+    value_t** arr;
+}matrix;
+
+
+typedef struct simulation{
+    init_t generations;
+    init_t freq;
+    init_t seed;
+    matrix* old;
+    matrix* 
+}simulation;
+
+
+/*void printArray(matrix* m){
+    for (size_t  i = 0; i < m->rows; i++){
+        for (int j = 0; j < m->cols; j++){
+            printf("%2d ", m->arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}*/
+
+
+/*
+uses cmd line arguments to setup variables for simulations
+*/ 
+int parseArgs(simulation* sim, index_t offset, char* argv[]){
+    if  ( sscanf(argv[offset], "%d", &sim->generations) != 1 ){
+        return 1;
+    }
+    if ( sscanf(argv[offset+1], "%d", &sim->freq) != 1 ){
+        return 1;
+    }
+    return 0;
+}
+
+
+/*
+allocates memory to hold 2d grid of numbers
+*/ 
+matrix* initMat(index_t rows, index_t cols){
+    matrix* mat = (matrix*) malloc(sizeof(matrix));
+    mat->rows = rows;
+    mat->cols = cols;
+    value_t** arr = (value_t**) malloc(sizeof(value_t*) * rows);
+    for (index_t i = 0; i < rows; i++){
+        arr[i] = (value_t*) calloc(sizeof(value_t), cols);
+    }
+    mat->arr = arr;
+    return mat;
+}
+
+
+/*
+reads file to set up grid
+exits if file has non-integer input
+*/ 
+matrix* readFile(FILE* fd){
+    char buf[1024];
+    char *p_rows, *p_cols, *p_n;
+    int rows, cols, n;
+    
+    // reads first line
+    fgets(buf, sizeof buf, fd);
+    p_rows = strtok(buf, " ");
+    p_cols = strtok(NULL, " ");
+    p_n = strtok(NULL, " ");
+
+    if (sscanf(p_rows, "%d", &rows) != 1){
+        fprintf(stderr, "Error: File contains non-integer input");
+        exit(EXIT_FAILURE);
+    }
+    if (sscanf(p_cols, "%d", &cols) != 1){
+        fprintf(stderr, "Error: File contains non-integer input");
+        exit(EXIT_FAILURE);
+    }
+    if (sscanf(p_n, "%d", &n) != 1){
+        fprintf(stderr, "Error: File contains non-integer input");
+        exit(EXIT_FAILURE);
+    }
+
+    matrix* mat = initMat(rows, cols);
+    int** arr = mat->arr;
+
+    // read each line and changes the value in arr
+    char *p_x, *p_y, *p_val;
+    int x, y, val;
+    for (int i = 0; i < n; i++){
+        fgets(buf, sizeof buf, fd);
+        p_x = strtok(buf, " ");
+        p_y = strtok(NULL, " ");
+        p_val = strtok(NULL, " ");
+        
+        if (sscanf(p_x, "%d", &x) != 1){
+            fprintf(stderr, "Error: File contains non-integer input");
+            exit(EXIT_FAILURE);
+        }
+        if (sscanf(p_y, "%d", &y) != 1){
+            fprintf(stderr, "Error: File contains non-integer input");
+            exit(EXIT_FAILURE);
+        }
+        if (sscanf(p_val, "%d", &val) != 1){
+            fprintf(stderr, "Error: File contains non-integer input");
+            exit(EXIT_FAILURE);
+        }
+
+        if (x < rows && y < cols && x >= 0 && y >= 0){
+            arr[x][y] = val;
+        }
+        else{
+            fprintf(stderr, "Error: File input outside bounds of matrix");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return mat;
+}
+
+value_t nodeUpdate(value_t** m, index_t i, index_t j){
+    value_t nalive = 0;
+    value_t isalive = m[i][j];
+
+    for (index_t testi = i-1; testi++; testi <= i+1){
+        for (index_t testj = j-1; testj++; testj <= j+1){
+            if (m[testi][testj] == 1){
+                alive++;
+            }
+        }
+    }
+
+
+    if (isalive && ( nalive == 2 || nalive == 3) ){
+        return 1;
+    }
+    else if (!isalive && (nalive == 3)){
+        return 1;
+    }
+    return 0;
+}
+
+
+/*
+updates each value in grid by adding a random value
+does not update the value if it falls outside min and max
+*/ 
+void update(matrix* m1, matrix* m2){
+
+    index_t r = m2->rows;
+    index_t c = m2->cols;
+    value_t** arr = m2->arr;
+
+    for (index_t i = 1; i < r-1; i++){
+        for (index_t j = 1; j < c-1; j++){
+            m1[i][j] = nodeUpdate(arr, i, j);
+        }
+    }
+
+}
+
+/*
+calls update and prints the array as needed
+*/ 
+void iterate(simulation* sim){
+
+    sim->
+
+
+    for (int i = 0; i < sim->generations; i++){
+
+        /*if (sim->freq != 0 && i % (sim->freq) == 0 ){
+            printf("Count: %d\n", i);
+            printf("------------\n");
+            printArray(mat);
+            printf("------------\n");
+        }*/
+        cont = update(mat, min, max);
+
+        if (! cont ){
+            break;
+        }
+    }
+
+    printf("final\n");
+    printf("------------\n");
+    printArray(mat);
+    printf("------------\n");
+}
+
+/*
+frees allocated memory
+*/ 
+void clean(simulation* sim){
+    matrix* mat = sim->mat;
+    for (int i = 0; i < mat->rows; i++){
+        free(mat->arr[i]);
+    }
+    free(mat->arr);
+    free(mat);
+    free(sim);
+}
+
+
+int main(int argc, char* argv[]){
+
+    // sim is an object that will store the state of our simulation
+    simulation* sim = (simulation*) malloc(sizeof(simulation));
+
+    if (argc != 3 && argc != 6){
+        fprintf(stderr, "Usage: %s generations frequency [seeed] [rows] [column] \n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // parse initial arguments
+    if ( parseArgs(sim, 1, argv) == 1){
+        fprintf(stderr, "Error: Input is not numbers");
+        exit(EXIT_FAILURE);
+    }
+
+    // calls readFile if there were only 5 cmd line arguments
+    if (argc == 3){
+        char filename[512]; 
+        printf("Matrix file path: ");
+        scanf("%512s", filename);
+        FILE* fd = fopen(filename, "r");
+        
+        if (fd == NULL){
+            fprintf(stderr, "Error opening file %s: %s \n", filename, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        sim->mat = readFile(fd);
+        fclose(fd);
+    }
+    // initializes grid of numbers to all 0
+    if (argc == 6){
+        int rows, cols;
+        if (sscanf(argv[5], "%d", &rows) != 1){
+            fprintf(stderr, "Error: Input is not numbers");
+            exit(EXIT_FAILURE);
+        }
+        if (sscanf(argv[6], "%d", &cols) != 1){
+            fprintf(stderr, "Error: Input is not numbers");
+            exit(EXIT_FAILURE);
+        }
+        sim->mat = initMat(rows, cols);
+    }
+
+    // seed for our random number generation
+    if (sim->seed < 0){
+        srand(time(NULL));
+    }
+    else {
+        srand(sim->seed);
+    }
+
+    // let the simulation run
+    iterate(sim);
+
+    // clean memory
+    clean(sim);
+
+    return 0;
+}
---
+++ lab02/gol.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <stdint.h>
+
+// this struct will store our matrix of numbers 
+typedef struct matrix{
+    size_t rows;
+    size_t cols;
+    uint8_t** arr;
+}matrix;
+
+// this struct will store the state of our simulation
+typedef struct simulation{
+    int iterations;
+    int threshold;
+    int freq;
+    int seed;
+    matrix* mat;
+}simulation;
+
+void printArray(matrix* m);
+int parseArgs(simulation* sim, int offset, char* argv[]);
+matrix* initMat(int rows, int cols);
+matrix* readFile(FILE* fd);
+short update(matrix* mat, int min, int max);
+void iterate(simulation* sim);
+void clean(simulation* sim);
