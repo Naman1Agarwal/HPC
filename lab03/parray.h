2fe7b94718a2c091f07ef78b30c0ce2fc482b843
naman <namanagarwalat@gmail.com>
2026-02-06 08:35:14 -0500

    update

---
+++ lab03/parray.h
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
+
+typedef struct simulation{
+    int iterations;
+    int threshold;
+    int freq;
+    int seed;
+    int threads;
+    int verbose;
+    
+    int rows; // 2d grid attributes
+    int cols;
+    int** arr;
+}simulation;
+
+typedef struct{
+    int id;
+    int start_col;
+    int end_col;
+}thread_args;
+
+#define FALSE 0
+#define TRUE 1
+
+
+int parseArgs(simulation* sim, int offset, char* argv[]);
+
+
+void initMat(simulation* sim, int rows, int cols);
+
+
+void printArray(simulation* sim);
+
+
+void readFile(FILE* fd, simulation* sim);
+
+
+void getMatFromUser(simulation* sim);
+
+
+void* iter(void* varg);
+
+
+void clean(simulation* sim);
