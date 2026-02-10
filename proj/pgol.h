29b410536cc9f812817553d403c116892429a167
Naman1Agarwal <m270036@usna.edu>
2026-02-10 09:15:41 -0500

    started proj 1

---
+++ proj/pgol.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <stdint.h>
+
+
+// uint8_t
+// uint32_t
+// int
+
+size_t generations;
+size_t freq;
+int seed;
+int verbose;
+uint8_t** old;
+uint8_t** new;
+size_t cols;
+size_t rows;
+
+int main(int argc, char* argv[]){
+
+    
+}
